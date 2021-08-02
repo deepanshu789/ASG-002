d3b94ea9db9d47a03ec9c44d3f7c72c9087409b3
Deepanshu <dsinghm98@gmail.com>
2021-08-02 12:55:20 +0530

    crud for brands

---
+++ WebApi/WebAPI/WebApplication1/Controllers/ProductionBrandsController.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class ProductionBrandsController : ApiController
+    {
+        public HttpResponseMessage Get()
+        {
+            string query = @"select brand_id, brand_name from production.brands";
+            DataTable table = new DataTable();
+            using (var con = new SqlConnection(ConfigurationManager.ConnectionStrings["BikeDB"].ConnectionString))
+                using (var cmd = new SqlCommand(query, con))
+            using (var da = new SqlDataAdapter(cmd))
+            {
+                cmd.CommandType = CommandType.Text;
+                da.Fill(table);
+            }
+            return Request.CreateResponse(HttpStatusCode.OK, table);
+
+        }
+
+        public string Post(ProductionBrands pro)
+        {
+            try
+            {
+                string query = @"insert into production.brands values('" + pro.brand_name + @"')";
+                DataTable table = new DataTable();
+                using (var con = new SqlConnection(ConfigurationManager.ConnectionStrings["BikeDB"].ConnectionString))
+                using (var cmd = new SqlCommand(query, con))
+                using (var da = new SqlDataAdapter(cmd))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    da.Fill(table);
+                }
+                return "Added Successfully";
+
+            }
+            catch (Exception e)
+            {
+                return "Failed to add";
+            }
+        }
+
+        public string Put(ProductionBrands pro)
+        {
+            try
+            {
+                string query = @"update production.brands set brand_name='" + pro.brand_name + @"'where brand_id = " + pro.brand_id + @"";
+                DataTable table = new DataTable();
+                using (var con = new SqlConnection(ConfigurationManager.ConnectionStrings["BikeDB"].ConnectionString))
+                using (var cmd = new SqlCommand(query, con))
+                using (var da = new SqlDataAdapter(cmd))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    da.Fill(table);
+                }
+                return "Updated Successfully";
+
+            }
+            catch (Exception e)
+            {
+                return "Failed to update";
+            }
+        }
+
+        public string Delete(int id)
+        {
+            try
+            {
+                string query = @"delete from production.brands where brand_id=" + id + @"";
+                DataTable table = new DataTable();
+                using (var con = new SqlConnection(ConfigurationManager.ConnectionStrings["BikeDB"].ConnectionString))
+                using (var cmd = new SqlCommand(query, con))
+                using (var da = new SqlDataAdapter(cmd))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    da.Fill(table);
+                }
+                return "Deleted Successfully";
+
+            }
+            catch (Exception e)
+            {
+                return "Failed to delete";
+            }
+        }
+
+    }
+}
---
+++ WebApi/WebAPI/WebApplication1/Models/ProductionBrands.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class ProductionBrands
+    {
+        public int brand_id { get; set; }
+        public string brand_name { get; set; }
+    }
+}
